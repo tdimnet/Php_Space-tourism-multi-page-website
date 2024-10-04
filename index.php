78005006478ebd3f8a36e4d6b95458e748a7260a
tdimnet <thomas.dimnet@gmail.com>
2024-10-04 08:13:06 +0200

    Starting to set up Sass architecture

--- index.php
+++ index.php
@@ -3,20 +3,15 @@
 require_once("./vendor/autoload.php");
 
 $request = $_SERVER['REQUEST_URI'];
-$viewDir = '/views/';
-
 
 $loader = new \Twig\Loader\FilesystemLoader('./views');
 $twig = new \Twig\Environment($loader);
-
-
 
 switch ($request) {
     case '':
     case '/':
         echo $twig->render('home.twig');
         break;
-
 
     case '/crew':
         echo $twig->render('crew.twig');
