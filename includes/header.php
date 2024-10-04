38b1e99cf9a1607e6ae5ea1e49fb6b5d36c36951
tdimnet <thomas.dimnet@gmail.com>
2024-10-04 06:32:21 +0200

    Adding basic html content

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,33 @@
-<nav>
-    <ul>
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/destination">Destination</a>
-        </li>
-        <li>
-            <a href="/crew">Crew</a>
-        </li>
-        <li>
-            <a href="/technology">Technology</a>
-        </li>
-    </ul>
-</nav>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Space Tourism Website</title>
+
+    <link rel="stylesheet" href="../public/css/main.css">
+
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/destination">Destination</a>
+                </li>
+                <li>
+                    <a href="/crew">Crew</a>
+                </li>
+                <li>
+                    <a href="/technology">Technology</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    
