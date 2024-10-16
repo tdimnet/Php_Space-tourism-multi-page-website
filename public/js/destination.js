e0456c40e3509881518d894fbd6d22fda919057d
tdimnet <thomas.dimnet@gmail.com>
2024-10-16 10:02:02 +0200

    Changing all planet info on click

--- public/js/destination.js
+++ public/js/destination.js
@@ -1,13 +1,28 @@
-const $planets = document.querySelectorAll('.destination-page__planet')
+const $planets = document.querySelectorAll(".destination-page__planet");
 
-function onChangePlanet() {
-    $planets.forEach($planet => {
-        $planet.addEventListener('click', () => {
-            $planets.forEach($planet => $planet.classList.remove('destination-page__planet--active'))
+function onChangePlanet($selectedPlanet) {
+  $planets.forEach(($planet) =>
+    $planet.classList.remove("destination-page__planet--active")
+  );
+  $selectedPlanet.classList.add("destination-page__planet--active");
 
-            $planet.classList.add('destination-page__planet--active')
-        })
-    })
+  const selectedPlanetData = PLANETS_DATA[$selectedPlanet.textContent.toLowerCase()]
+
+  const $planetImg = document.querySelector('.destination-page__planet-image')
+  const $planetName = document.querySelector('.destination-page__planet-name')
+  const $planetDescription = document.querySelector('.destination-page__planet-description')
+  const $planetAvgDistance = document.querySelector('.destination-page__avg-distance-value')
+  const $planetTravelTime = document.querySelector('.destination-page__travel-time-value')
+
+  $planetImg.src = selectedPlanetData.img
+  $planetName.textContent = selectedPlanetData.name
+  $planetDescription.textContent = selectedPlanetData.description
+  $planetAvgDistance.textContent = selectedPlanetData.avgDistance
+  $planetTravelTime.textContent = selectedPlanetData.travelTime
 }
 
-onChangePlanet()
+$planets.forEach(($planet) => {
+  $planet.addEventListener("click", () => {
+    onChangePlanet($planet);
+  });
+});
