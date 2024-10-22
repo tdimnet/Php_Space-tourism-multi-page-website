38e8a62c662059820c1a32ce5726f27f65f8e707
tdimnet <thomas.dimnet@gmail.com>
2024-10-22 18:07:47 +0200

    Updating event function for crew member

--- public/js/crew.js
+++ public/js/crew.js
@@ -1,7 +1,17 @@
-function onChangeCrewMember() {
+const $bulletsCrew = document.querySelectorAll('.crew-page__bullets-list')
+
+function onChangeCrewMember($bullet) {
+    console.log("======")
+    console.log($bullet)
     console.log("======")
     console.log("======")
+    console.log(CREW_DATA)
     console.log("======")
 }
 
-onChangeCrewMember()
+
+$bulletsCrew.forEach(($bullet) => {
+    $bullet.addEventListener('click', () => {
+        onChangeCrewMember($bullet)
+    })
+})
