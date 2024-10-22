31dd40330416ab20416966a6519af8e5df94af7f
tdimnet <thomas.dimnet@gmail.com>
2024-10-22 19:21:10 +0200

    Writing function when technology elt has been clicked

--- public/js/technology.js
+++ public/js/technology.js
@@ -1,3 +1,21 @@
-console.log("====")
-console.log("====")
-console.log("====")
+const $technologySteps = document.querySelectorAll('.technology-page__technology-step')
+
+function onChangeTechnology($selectedTechnology) {
+    $technologySteps.forEach($step => {
+        $step.classList.remove('technology-page__technology-step--active')
+    })
+    $selectedTechnology.classList.add('technology-page__technology-step--active')
+
+    console.log("====")
+    console.log($selectedTechnology)
+    console.log("====")
+    console.log("====")
+    console.log(TECHNOLOGIES_DATA)
+    console.log("====")
+}
+
+$technologySteps.forEach($step => {
+    $step.addEventListener('click', () => {
+        onChangeTechnology($step)
+    })
+})
